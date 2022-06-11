cb8c7f77d74d93d9e538447cfcdd332a2bf07de6
chandrakiran343 <chandra.dkiran@outlook.com>
2022-06-11 22:10:07 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './styles/index.css';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloProvider, ApolloClient, createHttpLink,InMemoryCache } from '@apollo/client';
 
+
+const httpLink = createHttpLink({url: 'http://localhost:4000'});
+const client = new ApolloClient({link: httpLink, cache: new InMemoryCache()});
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ApolloProvider client={client}>
     <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
